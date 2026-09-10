6a1d7d381091a02c9bcfe7f8ac3efb9a6b0e6f00
Sharon <sharonmmvii@gmail.com>
2026-09-10 13:18:19 +0530

    Add files via upload

---
+++ flipkart.sql
@@ -0,0 +1,19 @@
+create database flipkart;
+USE flipkart;
+create table Customer( c_id INT auto_increment, c_name varchar(50), email varchar(50) unique , phone varchar(20) unique , Address varchar(100) );
+desc Customer;
+insert into Customer(c_id,c_name,email,phone,Address)
+values(1001,"Sharon","sharon@gmail.com","9566731107","AnnaNagar"),
+(1002,"leo","eo@gmail.com","10000101010","3 avenue");
+
+ALTER TABLE Customer
+ADD PRIMARY KEY (C_id);
+
+UPDATE Customer
+SET address = 'universe 6'
+WHERE C_id = 106;
+
+DELETE FROM Customer
+WHERE C_id = 108;
+
+SELECT * FROM Customer;
