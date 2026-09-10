9ce36a89f0f7741cfdb917a5948bca34f2a288d7
Sharon <sharonmmvii@gmail.com>
2026-09-10 13:29:56 +0530

    Update flipkart1.sql

--- flipkart1.sql
+++ flipkart1.sql
@@ -4,8 +4,8 @@ create table Customer( c_id INT auto_increment, c_name varchar(50), email varcha
 desc Customer;
 insert into Customer(c_id,c_name,email,phone,Address)
 values(1001,"Sharon","sharon@gmail.com","9566731107","AnnaNagar"),
-(1002,"leo","leo@gmail.com","1234567890","10th avenue");
-(1003,"john","john@gmail.com","2345678901","3rd avenue");
+(1002,"leo","leo@gmail.com","1234567890","10th avenue"),
+(1003,"john","john@gmail.com","2345678901","3rd avenue"),
 (1004,"peter","peter@gmail.com","3456789012","4th avenue");
 
 ALTER TABLE Customer
